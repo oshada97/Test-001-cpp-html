68dc62f9fcd0fb4f774206af1a4930832f8edde9
Oshada Basnayake <oshadasinhalaya@gmail.com>
2019-05-06 22:39:07 +0530

    repetition 5

---
+++ repetition 5.cpp
@@ -0,0 +1,22 @@
+	#include <iostream>
+	
+	using namespace std;
+	
+	int main (){
+		
+		int x=1;
+		int sum=0;
+		
+		while (x<=5){
+			
+			sum=sum+x;
+			
+			x=x+1;
+
+		}
+		
+		cout<<sum<<endl;
+		
+		return 0;
+		
+	}
