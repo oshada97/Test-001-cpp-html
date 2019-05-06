c13d274b267a2f4a566f4444e2687f4ed919953c
Oshada Basnayake <oshadasinhalaya@gmail.com>
2019-05-06 22:21:33 +0530

    Add files via upload

---
+++ switch case cpp/1007 01.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	
+	char grade='B';
+	switch(grade){
+		case'A':cout<<"Its A";
+		break;
+		case'B':cout<<"Its B";
+		break;
+     	case'C':cout<<"Its C";
+     	break;
+		case'D':cout<<"Its D";
+     	break;
+	    default:cout<<"Its Another";
+	}return 0;
+}
