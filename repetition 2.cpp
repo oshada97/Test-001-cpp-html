76715d4b631c7df0d8d8a69eb9ec377ff1b88444
Oshada Basnayake <oshadasinhalaya@gmail.com>
2019-05-06 22:27:18 +0530

    repetition 2

---
+++ repetition 2.cpp
@@ -0,0 +1,19 @@
+	#include <iostream>
+	
+	using namespace std;
+	
+	int main (){
+		
+		int x=1;
+		
+		while (x<=5){
+			
+			cout<<"Hello"<<endl;
+			
+			x=x+1;
+			
+		}
+		
+		return 0;
+		
+	}
