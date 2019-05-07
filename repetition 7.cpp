7ecc6c1788705a46d3c8eef249e4a4b4dd7cc5c9
Oshada Basnayake <oshadasinhalaya@gmail.com>
2019-05-07 23:35:47 +0530

    repetition 7

---
+++ repetition 7.cpp
@@ -0,0 +1,19 @@
+	#include <iostream>
+	
+	using namespace std;
+	
+	int main (){
+		
+		int x=1;
+		
+		while (x<=10){
+			
+			cout<<" 7 x"<<x<<"="<<(7*x)<<endl;
+			
+			x=x+1;
+
+		}
+		
+		return 0;
+		
+	}
