453f223501c49e482aec6b4cea8ce03eb4a7cc5e
Oshada Basnayake <oshadasinhalaya@gmail.com>
2019-05-08 22:34:31 +0530

    function 1 (0923 1.cpp)

---
+++ 0923 1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void sayHello(){
+	
+	cout<<"HELLO"<<endl;
+
+}
+
+int main (){
+	
+	cout<<"before say HELLO"<<endl;
+	
+	sayHello();
+	
+	cout<<"after say HELLO"<<endl;
+	
+	return 0;
+	
+}
